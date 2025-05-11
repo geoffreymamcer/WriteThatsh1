c68b2002ff18d6629dabdeb6a25120c34aba7080
geoffreymamcer <115859560+geoffreymamcer@users.noreply.github.com>
2025-05-11 20:38:37 +0800

    Update configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Try this if you have routing issues
+  base: "/WriteThatsh1/", // Important: Add your repository name here
 });
